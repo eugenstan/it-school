e5526714d13245686d49a36948cbc734714c9f0e
eugenstan <d.eugen.stan@gmail.com>
2024-03-04 20:08:21 +0200

    Push by Ionut

--- session4/src/com/itschool/course/CourseMain4.java
+++ session4/src/com/itschool/course/CourseMain4.java
@@ -3,6 +3,7 @@ package com.itschool.course;
 public class CourseMain4 {
     public static void main(String[] arg) {
         System.out.println("Hello world!");
+        System.out.println("Hello from Ionut");
     }
 
 }
